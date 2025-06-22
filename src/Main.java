b4d4d899f4edd5a5b8a54e62d44cfe3edc33afad
egor <vlad.filin_2000@mail.ru>
2025-06-23 00:13:27 +0300

    6 zadanie

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        short time = 2;
-        short bottle = 16;
-        int perfomance = bottle / time;
-        System.out.println("За 20 минут машина произвела " + (perfomance*20) + " штук бутылок");
-        System.out.println("За сутки машина произвела " + (perfomance*60*24) + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + (perfomance*60*24*3) + " штук бутылок");
-        System.out.println("За месяц машина произвела " + (perfomance*60*24*30) + " штук бутылок");
+        int bananas = 5 * 80;
+        int milk = (200 / 100) * 105;
+        int iceCream = 2 * 100;
+        int eggs = 4 * 70;
+        float breakFast = (bananas + milk + iceCream + eggs) / 1000f;
+        System.out.println("Общий вес завтрака = " + breakFast);
         }
     }
