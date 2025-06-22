9a5f542d5b230941145b18b91916843f887060b2
egor <vlad.filin_2000@mail.ru>
2025-06-23 00:10:07 +0300

    2 zadanie

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int box = 123456;
-        byte car = 123;
-        short bear = 1234;
-        long stairs = 12345453535345346L;
-        float click = 123.345f;
-        double slow = 123.1235364363;
-        System.out.println("Значение переменной box с типом int равно " + box);
+        short box = 569;
+        byte car = 67;
+        short bear = -159;
+        long stairs = 987678965549L;
+        float click = 27.12f;
+        float slow = 2.786f;
+        short wait = 27897;
+        System.out.println("Значение переменной box с типом short равно " + box);
         System.out.println("Значение переменной car с типом byte равно " + car);
         System.out.println("Значение переменной bear с типом short равно " + bear);
         System.out.println("Значение переменной stairs с типом long равно " + stairs);
         System.out.println("Значение переменной click с типом float равно " + click);
         System.out.println("Значение переменной slow с типом double равно " + slow);
+        System.out.println("Значение переменной wait с типом short равно " + wait);
         }
     }
