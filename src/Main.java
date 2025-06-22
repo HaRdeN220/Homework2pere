22a933be9f73651b7865fa72a9106397a669163c
egor <vlad.filin_2000@mail.ru>
2025-06-23 00:14:08 +0300

    7 zadanie

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int bananas = 5 * 80;
-        int milk = (200 / 100) * 105;
-        int iceCream = 2 * 100;
-        int eggs = 4 * 70;
-        float breakFast = (bananas + milk + iceCream + eggs) / 1000f;
-        System.out.println("Общий вес завтрака = " + breakFast);
+        short mass1 = 250;
+        short mass2 = 500;
+        short massAll = 7 * 1000;
+        System.out.println("Если спортсмен будет худеть на 250 грамм за день у него уйдет " +
+                (massAll/mass1) + " дней");
+        System.out.println("Если спортсмен будет худеть на 500 грамм за день у него уйдет " +
+                (massAll/mass2) + " дней");
+        System.out.println("Если спортсмен будет худеть в среднем у него уйдет " +
+                (((massAll/mass1) + (massAll/mass2))/2) + " дня");
         }
     }
