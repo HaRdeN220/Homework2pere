6a688c01c5335e3b8c9e530d38150c17a5ba2d91
egor <vlad.filin_2000@mail.ru>
2025-06-23 00:14:49 +0300

    8 zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        short mass1 = 250;
-        short mass2 = 500;
-        short massAll = 7 * 1000;
-        System.out.println("Если спортсмен будет худеть на 250 грамм за день у него уйдет " +
-                (massAll/mass1) + " дней");
-        System.out.println("Если спортсмен будет худеть на 500 грамм за день у него уйдет " +
-                (massAll/mass2) + " дней");
-        System.out.println("Если спортсмен будет худеть в среднем у него уйдет " +
-                (((massAll/mass1) + (massAll/mass2))/2) + " дня");
+        public static void main(String[] args){
+            int salaryMasha1 = 67760;
+            float salaryMasha2 = salaryMasha1 * 1.1f;
+            int salaryDenis1 = 83690;
+            float salaryDenis2 = salaryDenis1 * 1.1f;
+            int salaryKristina1 = 76230;
+            float salaryKristina2 = salaryKristina1 * 1.1f;
+            System.out.println("Маша теперь получает " + salaryMasha2 + " рублей. Годовой доход вырос на " + ((salaryMasha2 - salaryMasha1)*12) +
+                    " рублей.");
+            System.out.println("Денис теперь получает " + salaryDenis2 + " рублей. Годовой доход вырос на " + ((salaryDenis2 - salaryDenis1)*12) +
+                    " рублей.");
+            System.out.println("Кристина теперь получает " + salaryKristina2 + " рублей. Годовой доход вырос на " + ((salaryKristina2 - salaryKristina1)*12) +
+                    " рублей.");
         }
     }
